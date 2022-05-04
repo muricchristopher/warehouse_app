18947ccf583af03a209c9d2a7e82b4bb6efff126
murichristopher <murichristopher@outlook.com>
2022-05-04 15:49:26 -0300

    Usuário cadastra um galpão com sucesso

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -2,4 +2,21 @@ class WarehousesController < ApplicationController
   def show
     @warehouse = Warehouse.find(params[:id])
   end
+
+  def new
+    
+  end
+
+  def create
+    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :address, :cep, :area, :description)
+
+    created_warehouse = Warehouse.new(warehouse_params)
+
+    created_warehouse.save()
+
+
+    redirect_to(root_path)
+  end
+
+
 end
---
+++ spec/system/user_register_warehouse_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe "Usuário cadastra um galpão" do
+  
+  it 'a partir da tela inicial' do
+    
+    visit(root_path)
+
+    click_on("Cadastrar Galpão")
+
+    expect(page).to have_field("Nome")
+    expect(page).to have_field("Descrição")
+    expect(page).to have_field("Endereço")
+    expect(page).to have_field("Código")
+    expect(page).to have_field("Cidade")
+    expect(page).to have_field("Área")
+    expect(page).to have_field("CEP")
+  end
+
+  it 'e com sucesso' do
+
+    visit(root_path)
+
+    click_on("Cadastrar Galpão")
+
+    fill_in "Nome",	with: "Galpão de Teste" 
+
+    click_on("Enviar")
+  
+    expect(current_path).to eq(root_path) 
+    expect(page).to have_content("Galpão de Teste") 
+  end
+
+end
